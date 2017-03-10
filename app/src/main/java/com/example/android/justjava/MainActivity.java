d0305900d8fe7a8888fa005972b149dc619bbf3f
allerasouza <allerasouza@gmail.com>
2017-03-10 19:55:44 -0300

    5) Create the String Variable

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -29,8 +29,11 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity*5);
+        String priceMessage = "Free";
+        displayMessage(priceMessage);
     }
+
+
 
     /**
      * This method is called when the plus button is clicked.
@@ -64,5 +67,14 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen
+     */
+
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
