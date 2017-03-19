66d67cf155beef256cc353cb57734cc030f81673
allerasouza <allerasouza@gmail.com>
2017-03-18 22:31:35 -0300

    9) Quiz: Syntax of if/else

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,6 +9,8 @@ package com.example.android.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -19,12 +21,14 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 0;
+    int quantity;
+    //int quantity = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        quantity = Integer.valueOf((((TextView) findViewById(R.id.quantity_text_view)).getText().toString()));
     }
 
     /**
@@ -42,7 +46,9 @@ public class MainActivity extends AppCompatActivity {
      * @return Returns Order Summary
      */
     private String createOrderSummary(int totalPrice) {
-        String priceMessage = "Name: Kaptain Kunal\n";
+        String priceMessage = "Name: " + (((EditText) findViewById(R.id.name_field)).getText().toString()) + "\n";
+        priceMessage += "Add whipped cream? " + ((CheckBox) findViewById(R.id.whipped_cream_checkbox)).isChecked() + "\n";
+        priceMessage += "Add chocolate? " + ((CheckBox) findViewById(R.id.chocolate_checkbox)).isChecked() + "\n";
         priceMessage += "Quantity: " + quantity + "\n";
         //message += "Total: $" + totalPrice + "\n";
         priceMessage += "Total: " + NumberFormat.getCurrencyInstance().format(totalPrice) + "\n";
