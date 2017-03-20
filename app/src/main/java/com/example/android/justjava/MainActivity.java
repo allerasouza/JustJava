0fd78898153951058376a40c36f394dace68fc7c
Aller Augusto de Souza <allerasouza@gmail.com>
2017-03-20 00:11:49 -0300

    9) Styles

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -6,12 +6,16 @@ package com.example.android.justjava;
  * package com.example.android.justjava;
  */
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+//import android.provider.AlarmClock;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -37,7 +41,29 @@ public class MainActivity extends AppCompatActivity {
     public void submitOrder(View view) {
         int price = calculatePrice();
         String priceMessage = createOrderSummary(price);
-        displayMessage(priceMessage);
+        //displayMessage(priceMessage);
+
+        /*Intent intent = new Intent(Intent.ACTION_SET_ALARM);
+        intent.setData(Uri.parse("geo:47.6, -122.3"));
+        if(intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);*/
+
+        /*Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
+        intent.putExtra(AlarmClock.EXTRA_MESSAGE, "AAS");
+        intent.putExtra(AlarmClock.EXTRA_HOUR, 20);
+        intent.putExtra(AlarmClock.EXTRA_MINUTES, 10);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }//<uses-permission android:name="com.android.alarm.permission.SET_ALARM" /> //AndroidManifest.xml
+        */
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        //intent.putExtra(Intent.EXTRA_SUBJECT, "JustJava order for " + ((EditText) findViewById(R.id.name_field)).getText().toString());
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.order_summary_name, ((EditText) findViewById(R.id.name_field)).getText().toString()));
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     /**
@@ -46,13 +72,12 @@ public class MainActivity extends AppCompatActivity {
      * @return Returns Order Summary
      */
     private String createOrderSummary(int totalPrice) {
-        String priceMessage = "Name: " + (((EditText) findViewById(R.id.name_field)).getText().toString()) + "\n";
-        priceMessage += "Add whipped cream? " + ((CheckBox) findViewById(R.id.whipped_cream_checkbox)).isChecked() + "\n";
-        priceMessage += "Add chocolate? " + ((CheckBox) findViewById(R.id.chocolate_checkbox)).isChecked() + "\n";
-        priceMessage += "Quantity: " + quantity + "\n";
-        //message += "Total: $" + totalPrice + "\n";
-        priceMessage += "Total: " + NumberFormat.getCurrencyInstance().format(totalPrice) + "\n";
-        priceMessage += "Thank you!";
+        String priceMessage = getString(R.string.order_summary_name, (((EditText) findViewById(R.id.name_field)).getText().toString())) + "\n";
+        priceMessage += getString(R.string.order_summary_whipped_cream, ((CheckBox) findViewById(R.id.whipped_cream_checkbox)).isChecked()) + "\n";
+        priceMessage += getString(R.string.order_summary_chocolate, ((CheckBox) findViewById(R.id.chocolate_checkbox)).isChecked()) + "\n";
+        priceMessage += getString(R.string.order_summary_quantity, quantity) + "\n";
+        priceMessage += getString(R.string.order_summary_price, NumberFormat.getCurrencyInstance().format(totalPrice)) + "\n";
+        priceMessage += getString(R.string.thank_you);
         return priceMessage;
     }
 
@@ -62,7 +87,12 @@ public class MainActivity extends AppCompatActivity {
      * @return total price
      */
     private int calculatePrice() {
-        return quantity * 5;
+        int basePrice = 5;
+        if(((CheckBox) findViewById(R.id.chocolate_checkbox)).isChecked())
+            basePrice += 2;
+        if(((CheckBox) findViewById(R.id.whipped_cream_checkbox)).isChecked())
+            basePrice += 1;
+        return quantity * basePrice;
     }
 
     /**
@@ -70,8 +100,11 @@ public class MainActivity extends AppCompatActivity {
      */
     public void increment(View view) {
         quantity = quantity + 1;
+        if (quantity > 100) {
+            quantity = 100;
+            Toast.makeText(this, "You cannot have more than " + quantity + " coffees", Toast.LENGTH_SHORT).show();
+        }
         displayQuantity(quantity);
-        //displayPrice(quantity*5);
     }
 
     /**
@@ -80,10 +113,11 @@ public class MainActivity extends AppCompatActivity {
     public void decrement(View view) {
 
         quantity = quantity - 1;
-        if (quantity < 0)
-            quantity = 0;
+        if (quantity < 1) {
+            quantity = 1;
+            Toast.makeText(this, "You cannot have less than " + quantity + " coffee", Toast.LENGTH_SHORT).show();
+        }
         displayQuantity(quantity);
-        //displayPrice(quantity*5);
     }
 
     /**
@@ -105,3 +139,4 @@ public class MainActivity extends AppCompatActivity {
 
 
 }
+
